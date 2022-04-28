fb986076bbc3e8ea92988bade97fe0f43876dc2f
Andreas2 <andreajn@stud.ntnu.no>
2022-04-28 16:12:37 -0700

    Test10

--- test/edu/berkeley/aep/project/CalculatorTest.java
+++ test/edu/berkeley/aep/project/CalculatorTest.java
@@ -14,9 +14,9 @@ public class CalculatorTest {
     }
 
     @Test
-    public void MinusTenPlusTwoEqualsMinusEight() {
-        Calculator calculator = new Calculator(-10, 2);
-        assertEquals(-8,calculator.addition(),0.1);
+    public void MinusTenPlusTwoPointTwoEqualsMinus7Point8() {
+        Calculator calculator = new Calculator(-10, 2.2);
+        assertEquals(-7.8,calculator.addition(),0.1);
     }
 
     @Test
@@ -26,26 +26,32 @@ public class CalculatorTest {
     }
 
     @Test
-    public void MinusThreeMinusMinusFiveEqualsTwo() {
-        Calculator calculator = new Calculator(-3, -5);
-        assertEquals(2,calculator.subtraction(),0.1);
+    public void MinusThreeMinusMinus5Point5Equals2Point5() {
+        Calculator calculator = new Calculator(-3, -5.5);
+        assertEquals(2.5,calculator.subtraction(),0.1);
     }
 
     @Test
     public void ThreeMultipliedWithThreeEqualsNine() {
-        Calculator calculator = new Calculator(3d, 3d);
-        assertEquals(9d,calculator.multiply(),0.1);
+        Calculator calculator = new Calculator(3, 3);
+        assertEquals(9,calculator.multiply(),0.1);
+    }
+
+    @Test
+    public void ThreeMultipliedWith5Point5Equals16Point5() {
+        Calculator calculator = new Calculator(3, 5.5);
+        assertEquals(16.5,calculator.multiply(),0.1);
     }
 
     @Test
     public void TenDividedWithTwoEqualsFive() {
-        Calculator calculator = new Calculator(10d, 2d);
-        assertEquals(5d,calculator.divide(),0.1);
+        Calculator calculator = new Calculator(10, 2);
+        assertEquals(5,calculator.divide(),0.1);
     }
 
     @Test
     public void TenDividedWithThreeEqualsThreePoint33() {
-        Calculator calculator = new Calculator(10d, 3d);
+        Calculator calculator = new Calculator(10, 3);
         assertEquals(3.33d,calculator.divide(),0.1);
     }
 
